5b57a50fa8372c09db31bdd60c8df078bcbc9564
dev2820 <second28200@naver.com>
2024-10-01 17:35:16 +0900

    refactor: change color of trds's theme

--- src/components/post/element/Anchor.tsx
+++ src/components/post/element/Anchor.tsx
@@ -7,7 +7,5 @@ export type AnchorProps = LinkProps;
 export function Anchor(props: AnchorProps) {
   const { className, ...rest } = props;
 
-  return (
-    <Link className={cx("font-medium text-primary", className)} {...rest} />
-  );
+  return <Link className={cx("font-medium", className)} {...rest} />;
 }
