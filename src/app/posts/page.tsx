c3a4c8928e186ee27fdca07e4af1c1b101495fd4
dev2820 <second28200@naver.com>
2024-10-20 02:06:00 +0900

    feat: post order 기능 추가

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -14,13 +14,25 @@ import { isFailed, isNil } from "@/utils/predicate";
 import { fetchPostListForSearch } from "@/utils/search";
 import Link from "next/link";
 import { MouseEvent, useCallback, useState } from "react";
+import { Select } from "terra-design-system/react";
 const BASE_PATH = process.env.basePath ?? "";
 
 const ALL_POSTS = "전체 보기";
-
+const orderOptions = ["최신순", "오래된순"];
+const compareFnTable = {
+  // 최신순
+  [orderOptions[0]]: (a: Post, b: Post) => {
+    return a.data.published < b.data.published ? 1 : -1;
+  },
+  // 오래된순
+  [orderOptions[1]]: (a: Post, b: Post) => {
+    return a.data.published > b.data.published ? 1 : -1;
+  },
+};
 export default function PostsPage() {
   const [posts, setPosts] = useState<Post[]>([]);
   const [currentFilter, setCurrentFilter] = useState<string>(ALL_POSTS);
+  const [currentOrder, setCurrentOrder] = useState<string>(orderOptions[0]);
   const filterOptions: [string, number][] = [
     [ALL_POSTS, posts.length],
     ...posts
@@ -37,11 +49,7 @@ export default function PostsPage() {
       if (currentFilter === ALL_POSTS) {
         return true;
       }
-      console.log(
-        post.data.tags,
-        currentFilter,
-        post.data.tags.includes(currentFilter)
-      );
+
       return post.data.tags.includes(currentFilter);
     },
     [currentFilter]
@@ -68,10 +76,17 @@ export default function PostsPage() {
     }
     setCurrentFilter(filterOption);
   };
+  const handleChangeSelect = (changedDetails: {
+    value: string[];
+    items: (object | string)[];
+  }) => {
+    const [value] = changedDetails.value;
+    setCurrentOrder(value);
+  };
   return (
     <>
       <PageHeading>Posts</PageHeading>
-      <ul className="flex flex-row gap-3 flex-wrap mb-4">
+      <ul className="flex flex-row gap-3 flex-wrap mb-16">
         {filterOptions.map(([filterOption, count]) => (
           <li key={filterOption}>
             <button onClick={handleClickTag} data-filter-option={filterOption}>
@@ -88,36 +103,50 @@ export default function PostsPage() {
           </li>
         ))}
       </ul>
-      {/**
-       * 정렬 기능
-       */}
-
+      <div className="mb-4">
+        <Select.Root
+          className="w-36 ml-auto"
+          items={orderOptions}
+          size="sm"
+          defaultValue={[orderOptions[0]]}
+          onValueChange={handleChangeSelect}
+        >
+          {orderOptions.map((order) => (
+            <Select.Item key={order} item={order}>
+              {order}
+            </Select.Item>
+          ))}
+        </Select.Root>
+      </div>
       <ul className="w-full">
-        {posts.filter(filterFn).map((post) => (
-          <li
-            key={post.data.title}
-            className="mb-4 last:mb-0 border-b first:border-t"
-          >
-            <SearchResultRoot className="p-4">
-              <Link
-                href={`/posts/${post.data.slug}`}
-                className="hover:underline"
-              >
-                <SearchResultTitle>{post.data.title}</SearchResultTitle>
-              </Link>
-              <SearchResultDescription>
-                {post.data.summary}
-              </SearchResultDescription>
-              <div className="mt-4 flex flex-row flex-wrap gap-1">
-                {post.data.tags.map((tag) => (
-                  <Link href={`${BASE_PATH}/tags/${tag}`} key={tag}>
-                    <Tag theme="secondary">{tag}</Tag>
-                  </Link>
-                ))}
-              </div>
-            </SearchResultRoot>
-          </li>
-        ))}
+        {posts
+          .filter(filterFn)
+          .toSorted(compareFnTable[currentOrder])
+          .map((post) => (
+            <li
+              key={post.data.title}
+              className="mb-4 last:mb-0 border-b first:border-t"
+            >
+              <SearchResultRoot className="p-4">
+                <Link
+                  href={`/posts/${post.data.slug}`}
+                  className="hover:underline"
+                >
+                  <SearchResultTitle>{post.data.title}</SearchResultTitle>
+                </Link>
+                <SearchResultDescription>
+                  {post.data.summary}
+                </SearchResultDescription>
+                <div className="mt-4 flex flex-row flex-wrap gap-1">
+                  {post.data.tags.map((tag) => (
+                    <Link href={`${BASE_PATH}/tags/${tag}`} key={tag}>
+                      <Tag theme="secondary">{tag}</Tag>
+                    </Link>
+                  ))}
+                </div>
+              </SearchResultRoot>
+            </li>
+          ))}
       </ul>
     </>
   );
