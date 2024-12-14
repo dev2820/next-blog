0be5c3f61d67f318ae74a252db151359c5c6b05a
dev2820 <second28200@naver.com>
2024-12-14 11:47:22 +0900

    feat: Blog Heading에 모바일 사이즈 고려

--- src/components/PageHeading.tsx
+++ src/components/PageHeading.tsx
@@ -5,6 +5,9 @@ export type PageHeadingProps = ComponentProps<"h2">;
 export function PageHeading(props: PageHeadingProps) {
   const { className, ...rest } = props;
   return (
-    <h2 className={cx("text-5xl font-bold mt-16 mb-8", className)} {...rest} />
+    <h2
+      className={cx("text-4xl font-bold mt-16 mb-8 lg:text-5xl", className)}
+      {...rest}
+    />
   );
 }
