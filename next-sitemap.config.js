be0f4218d297769c0039259cbd70aa5d06cc4af3
dev2820 <second28200@naver.com>
2024-12-26 20:17:05 +0900

    fix: sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: "https://terra-dev.me/",
+  siteUrl: "https://www.terra-dev.me/",
   changefreq: "daily",
   priority: 0.7,
   sitemapSize: 7000,
