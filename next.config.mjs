21adf2a23853b51e66bcc5c7e382270ebf94ee85
dev2820 <second28200@naver.com>
2024-12-26 20:36:42 +0900

    chore: update config

--- next.config.mjs
+++ next.config.mjs
@@ -8,12 +8,9 @@ const nextConfig = {
     ...config,
     NEXT_PUBLIC_GOOGLE_SEARCH_CONSOLE:
       process.env.NEXT_PUBLIC_GOOGLE_SEARCH_CONSOLE,
+    NEXT_PUBLIC_NAVER_SEARCH_CONSOLE:
+      process.env.NEXT_PUBLIC_NAVER_SEARCH_CONSOLE,
   },
-  rewrites: [
-    { source: "/sitemap.xml", destination: "/sitemap.xml" },
-    { source: "/robots.txt", destination: "/robots.txt" },
-    { source: "/((?!sitemap\\.xml|robots\\.txt).*)", destination: "/" },
-  ],
   trailingSlash: true,
 };
 
