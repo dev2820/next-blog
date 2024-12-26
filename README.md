671d3c4f08dcfebc639005cd4fecedbd1699cbce
dev2820 <second28200@naver.com>
2024-12-26 18:52:11 +0900

    chore: sitemap setup

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Next Blob
 
-빌딩중...
+https://terra-dev.me/
+
+## 참고
+
+배포 주소 변경시 `next-sitemap.config.js`와 `config.js` 둘 다함바꿔야함
 
 ## TODO
 
---
+++ next-sitemap.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: "https://terra-dev.me/",
+  changefreq: "daily",
+  priority: 0.7,
+  sitemapSize: 7000,
+  generateRobotsTxt: true,
+  exclude: [],
+};
+
+export default config;
