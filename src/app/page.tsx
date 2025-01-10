67b2e107a04e628e30dfa6028685208e6cebf9aa
dev2820 <second28200@naver.com>
2025-01-11 00:37:20 +0900

    fix: LandingPage에 Profile추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,11 @@ import { PageHeading } from "@/components/PageHeading";
 import { SearchResult } from "@/components/search/SearchResult";
 import { getAllPosts } from "@/utils/post";
 import { SeeMorePosts } from "@/components/SeeMorePosts";
+// import profilePng from "@/assets/images/profile.png";
+import profilePng from "@/assets/images/profile-transparent.png";
+import smileSvg from "@/assets/smile.svg";
+import parrotGif from "@/assets/images/rightparrot.gif";
+import Image from "next/image";
 
 export default function HomePage() {
   const recentPosts = getAllPosts()
@@ -11,6 +16,24 @@ export default function HomePage() {
     .slice(0, 3);
   return (
     <>
+      <div className="relative">
+        <Image
+          src={profilePng}
+          className="w-56 h-56 mt-20 rounded-full bg-white border-2"
+          alt={"terra's avatar"}
+        />
+        <div
+          aria-hidden
+          className="absolute bottom-3 left-3 p-1 border bg-white rounded-full text-center overflow-hidden"
+        >
+          <Image
+            src={parrotGif}
+            aria-hidden
+            alt="smile"
+            className="size-8 p-1px rounded-full"
+          />
+        </div>
+      </div>
       <PageHeading>
         <span className="text-primary">Terra</span>의 개발 블로그
       </PageHeading>
