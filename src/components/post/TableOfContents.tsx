7e28ac4e972d44dc635380aaf2d5d8ea4dd4d88c
dev2820 <second28200@naver.com>
2025-01-19 15:59:37 +0900

    feat: toc active시 scale up

--- src/components/post/TableOfContents.tsx
+++ src/components/post/TableOfContents.tsx
@@ -72,7 +72,7 @@ const Item = forwardRef<HTMLDivElement, ItemProps>(function Item(props, ref) {
   return (
     <div
       className={cx(
-        'text-sm font-light py-0.5 text-neutral-400 last:data-[active="true"]:text-primary duration-200 hover:text-primary',
+        'text-sm font-light py-0.5 text-neutral-400 data-[active="true"]:scale-105 data-[active="true"]:text-primary duration-200 hover:text-primary',
         item.level === 1 && "pl-2",
         item.level === 2 && "pl-4",
         item.level === 3 && "pl-10",
