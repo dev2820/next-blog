07f0882dbfcf56ed75747eef70ce8942af1da5e3
dev2820 <second28200@naver.com>
2025-01-22 10:50:40 +0900

    fix: dark mode 동작 개선

--- src/utils/darkmode.ts
+++ src/utils/darkmode.ts
@@ -2,7 +2,11 @@ const DARKMODE_KEY = "_darkMode";
 
 export const changeMode = (isDarkmode: boolean) => {
   localStorage.setItem(DARKMODE_KEY, isDarkmode ? "true" : "false");
-  document.documentElement.classList.toggle("dark");
+  if (isDarkmode && !document.documentElement.classList.contains("dark")) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
 };
 export const isDarkMode = () => {
   const persist = localStorage?.getItem(DARKMODE_KEY);
