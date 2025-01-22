bd6c836adfc8b9042abebe866ca1165a9619a7fe
dev2820 <second28200@naver.com>
2025-01-22 22:01:14 +0900

    refactor: remove debug code

--- src/utils/darkmode.ts
+++ src/utils/darkmode.ts
@@ -1,10 +1,8 @@
 const DARKMODE_KEY = "_darkMode";
 
 export const changeMode = (isDarkmode: boolean) => {
-  console.log(isDarkmode);
   localStorage.setItem(DARKMODE_KEY, isDarkmode ? "true" : "false");
   if (isDarkmode && !document.documentElement.classList.contains("dark")) {
-    console.log("?");
     document.documentElement.classList.add("dark");
   } else if (
     !isDarkmode &&
