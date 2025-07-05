3264d84d13417b5e7238c7289a67e95738010a55
dev2820 <second28200@naver.com>
2025-07-05 22:23:15 +0900

    fix: caption은 같은 색을 유지하게 만듦

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -102,7 +102,7 @@ export default async function PostPage({ params }: PageProps) {
         <Emphasize
           {...props}
           // for caption
-          className="[img+&]:block [img+&]:text-center [img+&]:text-fg-caption [img+&]:font-normal [img+&]:not-italic [img+&]:text-sm [img+&]:-translate-y-10"
+          className="[img+&]:block [img+&]:text-center [img+&]:text-fg-caption! [img+&]:font-normal [img+&]:not-italic [img+&]:text-sm [img+&]:-translate-y-10"
         />
       ),
       strong: Strong,
